8f0dc680d50067a28dc09d3d6a6b794ec7928fd0
pratyush <guptapratyush98@gmail.com>
2022-06-30 22:34:55 +0530

    Selection Sort example added

---
+++ sorting_algorithms/selection_sort.py
@@ -0,0 +1,32 @@
+'''
+Selection Sort:
+https://www.geeksforgeeks.org/selection-sort/
+It is the opposite of bubble sort from what we understand.
+In Bubble sort, we optimised to place the largest number by performing swaps at the end of the array.
+In Insertion sort, we tend to optimise for the smalled number by swapping it with the smallest number declared dynamically.
+'''
+
+### general implementation
+
+def insertionSort(arr, N):
+    for i in range(N):
+        minIndex = i
+        
+        '''
+        Setting the first element as the smallest,
+        Running the second loop to determine the smallest
+        At the end performing the swap
+        '''
+        for j in range(i + 1, N):
+            if (arr[minIndex] > arr[j]): ### check if the minIndex is the smallest or not
+                minIndex = j
+
+        arr[minIndex], arr[i] = arr[i], arr[minIndex]
+
+    return arr 
+
+testArray = [90, 30, 45, 23, 56, 66, 92, 99]
+
+resultArray = insertionSort(testArray, len(testArray))
+
+print(f"This is the input array {testArray}, and this is the output array {resultArray}")
