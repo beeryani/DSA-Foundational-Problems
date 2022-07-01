ff18332cdab15ef28d731f65c59cb8a284ed598b
pratyush <guptapratyush98@gmail.com>
2022-07-01 17:09:33 +0530

    minor changes

--- sorting_algorithms/bubble_sort.py
+++ sorting_algorithms/bubble_sort.py
@@ -16,7 +16,7 @@ def bubbleSort(arr):
     n = len(arr)
 
     for i in range(n):  ### important correction to the approach
-        for j in range(n - i - 1):
+        for j in range(n - i - 1): 
             if (arr[j] > arr[j + 1]):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     
