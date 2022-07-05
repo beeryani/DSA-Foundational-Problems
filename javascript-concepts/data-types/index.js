5c563db7bb76ecfdf721a05bf1cc39f9ece09018
pratyush <guptapratyush98@gmail.com>
2022-07-05 21:18:35 +0530

    Tasks added to better grasp concepts around variable scope and closure

--- javascript-concepts/data-types/index.js
+++ javascript-concepts/data-types/index.js
@@ -22,3 +22,33 @@ function showName (firstName, lastName, ...title) {
 }
 // the spread operator helps in tackling 
 console.log(showName("Pratyush", "Gupta", "Major", "General", "Janitor", "Data Cleaner", " "))
+
+
+// Object Destructuring     
+var assert = require('assert');
+
+let options = {
+    title: "Chinmay",
+    width: 200,
+    height: 400
+}
+//
+console.log(options)
+// extracting title and width properties out of the object "options"
+// let {title, width} = options
+// console.log(title)
+//
+let {title, ...rest} = options
+console.log(rest)
+
+//few exercises on destructuring 
+
+let user = {
+    name: "John",
+    years: 23
+}
+
+let {name, years: age, isAdmin = false} = user
+console.log(age)
+console.log(name)
+console.log(isAdmin)
