e178f8093a774e7858254f55be8af00e3821ce97
pratyush <guptapratyush98@gmail.com>
2022-07-09 17:42:08 +0530

    Striver Video Series added to comments

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -1,6 +1,7 @@
 '''
 Introduction to recursion:
 When a function can be expressed as a form of itself, it is called recursion.
+https://www.youtube.com/watch?v=yVdKa8dnKiE&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9
 '''
 
 ## Question 1: Print 1 to N without Loop
