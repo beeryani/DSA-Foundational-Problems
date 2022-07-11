81b543e76bd85b7f7bc2c0ddeb63eb993da7d5e0
pratyush <guptapratyush98@gmail.com>
2022-07-11 15:14:25 +0530

    Naive Solutions added to the Problem 4

--- leetcode_problems/day_1_four_problems.py
+++ leetcode_problems/day_1_four_problems.py
@@ -2,7 +2,8 @@
 # general theme on two pointer problems
 Problem no 1: https://leetcode.com/problems/merge-sorted-array/
 Problem no 2: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
-
+Problem no 3: https://leetcode.com/problems/valid-palindrome/
+Problem no 4: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 '''
 '''
 Applying two pointer approach:
@@ -50,8 +51,82 @@ def twoSum(numbers, target):
             return [start + 1, end + 1]
         else:
             start += 1;
-            
-    
-
 
 print(twoSum([2,7,11,15],9))
+
+'''
+Problem 3 of the day: Valid Palindrome
+Optimal Solution in one iteration
+'''
+
+import re
+
+def isPalindrome(s):
+    s = re.sub('[^A-Za-z0-9]+', '', s)
+    s = s.lower()
+
+    if (len(s) == 0):
+        return True
+    
+    start, end = 0, len(s) - 1;
+
+    while (end > start):
+        if (s[start] == s[end]):
+            end -= 1;
+            start += 1;
+        elif (s[start] != s[end]):
+            return False
+        
+    return True
+
+
+s = "kayak"
+
+print(f"{isPalindrome(s)}")
+
+'''
+Problem 4 of the day: Remove Duplicates from the Sorted Array
+Solution Iteration No 1:
+def removeDuplicates(nums):
+    start, end = 0, len(nums) - 1
+    k = 0
+
+    while (end > start):
+        if (nums[start] == nums[end]):
+            del nums[end];
+            nums.append("_");
+            start += 1;
+            k += 1;
+            end = len(nums) - 1
+        elif(nums[start] != nums[end]):
+            end -= 1;
+    
+    return nums, k
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+'''
+
+
+def removeDuplicates(nums):
+    start = 0
+    k = 0
+
+    while (start < len(nums) - 1):
+        end = len(nums) - 1;
+        while (start < end):
+            if (nums[end] == nums[start]):
+                del nums[end];
+                nums.append("_");
+                end -= 1;
+                k += 1;
+            else:
+                end -= 1;
+        start += 1;
+        
+        return nums, k
+
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(f"{removeDuplicates(nums)}")
+
+
